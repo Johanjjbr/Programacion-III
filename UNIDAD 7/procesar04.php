eeafb39e85954727c0c3b3196f669437a6656931
Johanjjbr <johanjesus1@gmail.com>
2026-05-04 18:30:26 -0300

    unidad 7 ejercicio 4

---
+++ UNIDAD 7/procesar04.php
@@ -0,0 +1,32 @@
+<doctype html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Ejercicio 3 - Procesar formulario</title>
+</head>
+<body>
+    <h2>Ejercicio 2: Validación de Datos</h2>
+    <h1>Datos recibidos desde página formulario03</h1>
+
+    <?php
+    $a = $_POST["mail"];
+    $b = $_POST["pass"];
+    $c = $_POST["tel"];
+
+
+    // Validación de la contraseña
+    if ($b == "programar") {
+        echo "<h2>Contraseña correcta</h2>";
+        echo "<p>El correo ingresado es: $a </p>";
+        echo "<p>La contraseña es ($b) </p>";
+        echo "<p>El teléfono ingresado es: $c </p>";
+    } else {
+        echo "<h2>La contraseña es incorrecta: $b </h2>";
+    }
+    ?>
+
+    <br>
+    <h3>Alumno: Johan Brito | Materia: Programación III</h3>
+    <h3>Año: 2026</h3>
+</body>
+</html>
