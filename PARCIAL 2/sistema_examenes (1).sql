211587316111c833eae3f1c9ee7e84a416d94952
Johanjjbr <johanjesus1@gmail.com>
2026-06-10 12:28:03 -0300

    parcial 2 sql corregido

--- PARCIAL 2/sistema_examenes (1).sql
+++ PARCIAL 2/sistema_examenes (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 10-06-2026 a las 17:25:31
+-- Tiempo de generación: 10-06-2026 a las 17:27:33
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
