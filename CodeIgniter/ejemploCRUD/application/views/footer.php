a5135c4f330d97a112de95f3c1ab6b2f459def6a
Johanjjbr <johanjesus1@gmail.com>
2026-06-23 13:35:28 -0300

    8.2 end

--- CodeIgniter/ejemploCRUD/application/views/footer.php
+++ CodeIgniter/ejemploCRUD/application/views/footer.php
@@ -2,7 +2,7 @@
    <hr/>
     <footer>
 <center>
-        <h6> &copy; Desarrollos PHP - Editado para Programación 3 - Soporta PHP Version 7 hasta 8.2.12 </h6>
+        <h6> Johan Brito -  Programación 3 - TSDS </h6>
     
    </center>
     </footer>
